bcbc8dfc1af432222b27313a9729b3c4ff2c4bc5
Nikodem Wrosz <wrosznikodem@gmail.com>
2020-11-20 23:31:22 +0100

    heroku settings

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -93,6 +93,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'bookstore_project.urls'
@@ -173,4 +174,4 @@ STATICFILES_FINDERS = [
 
 AUTH_USER_MODEL = 'users.CustomUser' #new
 
-
+django_heroku.settings(locals())
